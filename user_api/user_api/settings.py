2c3689f6ef85c6a5f62730958bfff72d49bf75e4
Rohan pagare <rohan173422@gmail.com>
2025-07-28 13:34:14 +0530

    Update settings.py

--- user_api/user_api/settings.py
+++ user_api/user_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "user_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "usermanagment",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
